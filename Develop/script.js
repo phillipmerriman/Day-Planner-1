00b5db135fe60f093489b7729cd4f1b5585b054e
Phillip Merriman <phillipmerriman@gmail.com>
2020-10-26 13:59:36 -0500

    updated timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,15 @@ create input fields for time blocks
 create save button for each time block
 link current time to time blocks with Moment.js
 set color for past time blocks, present time blocks, future time blocks
+    if time has passed
+        add class .past
+    if time is present
+        add class .present
+    if time is future
+        add class .future
 create click event for save button
-    save text from input field to localStorage
+    save text from input field to localStorage (localStorage.setItem("name", value))
+    render text from localStorage to event field (localStorage.getItem("name from setItem ↑ "))
 */
 
 //render current day at top of planner
@@ -15,6 +22,7 @@ setInterval(function () {
     }, 1000);
 
 //event listener for clicked save button
-$("button").on("click", function(){
+$(".submit").on("click", function(){
     console.log(`${this.id} event submitted!`);
+    // console.log(this.id.value);
 });
