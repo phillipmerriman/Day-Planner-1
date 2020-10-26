0432b773ebd48d6df730b9ea667afaf8640b51ab
Phillip Merriman <phillipmerriman@gmail.com>
2020-10-26 10:44:00 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+/*
+render current day at top of planner
+create time blocks in html (assignment requires regular biz hours, but maybe just do all hours?)
+create input fields for time blocks
+create save button for each time block
+link current time to time blocks with Moment.js
+set color for past time blocks, present time blocks, future time blocks
+create click event for save button
+    save text from input field to localStorage
+*/
